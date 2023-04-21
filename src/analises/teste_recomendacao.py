cb4da9f05073df3a86ab0472aee9571494e533b4
Gabriel Henrique <gabrielxdf16@gmail.com>
2023-04-21 11:11:36 -0300

    inclusão dos testes de recomendação

---
+++ src/analises/teste_recomendacao.py
@@ -0,0 +1,5 @@
+import recommendation as r
+
+
+#print(r.process_candidato_tfidf('./curriculos/curriculo_administracao_1.pdf'))
+print(r.process_candidato_bert('./curriculos/curriculo_administracao_1.pdf'))
